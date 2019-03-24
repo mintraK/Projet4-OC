60984f7766c5e410d14612012fc879de520c7424
mintra <studiominnie@gmail.com>
2019-03-25 00:00:54 +0100

    connexion.php fonctionalité pour user admin

--- connexion.php
+++ connexion.php
@@ -1,3 +1,45 @@
+<?php
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+if(isset($_POST['pseudo']) && isset($_POST['pwd'])){
+  $conn = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+  $pseudo = $_POST['pseudo'];
+
+  //  Récupération de l'utilisateur et de son pass hashé
+  $req = $conn->prepare('SELECT id, pwd FROM membres WHERE pseudo = :pseudo');
+  $req->execute(array(
+      'pseudo' => $pseudo));
+  $resultat = $req->fetch();
+  
+  // Comparaison du pass envoyé via le formulaire avec la base
+  $isPasswordCorrect = password_verify($_POST['pwd'], $resultat['pwd']);
+  
+  if (!$resultat)
+  {
+      echo 'Mauvais identifiant ou mot de passe !';
+  }
+  else
+  {
+      if ($isPasswordCorrect) {
+          session_start();
+          $_SESSION['id'] = $resultat['id'];
+          $_SESSION['pseudo'] = $pseudo;
+         
+          // echo 'Vous êtes connecté !';
+          header("Location:index.php");
+      }
+      else {
+          echo 'Mauvais identifiant ou mot de passe !';
+      }
+  }
+
+
+}
+else{
+
+}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -62,44 +104,7 @@
 </fieldset>
 </form>
 <?php  
-  if(isset($_POST['pseudo']) && isset($_POST['pwd'])){
-    $conn = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-    $pseudo = $_POST['pseudo'];
-
-    //  Récupération de l'utilisateur et de son pass hashé
-    $req = $conn->prepare('SELECT id, pwd FROM membres WHERE pseudo = :pseudo');
-    $req->execute(array(
-        'pseudo' => $pseudo));
-    $resultat = $req->fetch();
-    
-    // Comparaison du pass envoyé via le formulaire avec la base
-    $isPasswordCorrect = password_verify($_POST['pwd'], $resultat['pwd']);
-    
-    if (!$resultat)
-    {
-        echo 'Mauvais identifiant ou mot de passe !';
-    }
-    else
-    {
-        if ($isPasswordCorrect) {
-            session_start();
-            $_SESSION['id'] = $resultat['id'];
-            $_SESSION['pseudo'] = $pseudo;
-           
-            // echo 'Vous êtes connecté !';
-            header("Location:index.php");
-        }
-        else {
-            echo 'Mauvais identifiant ou mot de passe !';
-        }
-    }
-
-
-  }
-  else{
-
-  }
-
+  
 
 ?>
      <br/> <br/>
