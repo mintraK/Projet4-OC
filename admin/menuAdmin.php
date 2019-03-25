bbcfb2ad03c230c1e3fcf6c61b90f15bf80ed5cb
mintra <studiominnie@gmail.com>
2019-03-25 22:44:51 +0100

    menuadmin.php  enlever le menu verticale

--- admin/menuAdmin.php
+++ admin/menuAdmin.php
@@ -17,22 +17,5 @@
         </div>  
     </div>
     </header>
-  <br/>
-  <br/>
-  
-<div class="row">  
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">                
-                <ul class="nav navbar-inverse  flex-column">
-                     <li class="nav-item ">
-                         <a class="nav-link active" href="ajouteArticle.php">Ajouter un article</a>
-                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="supprimeArticle.php">Supprimer les articles</a>
-                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link" href="commentaireSignaler.php">Commentaires signalés</a>
-                    </li>
-             
-                 </ul>
-        </div>
-    <!-- </div> se trouve dans html -->
+ 
+ 
