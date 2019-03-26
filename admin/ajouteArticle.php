a16ea13a52a2caaa102195108900662c3b5e7f27
mintra <studiominnie@gmail.com>
2019-03-26 01:16:50 +0100

    ajouteArticle.php Ajouter TinyMCE

--- admin/ajouteArticle.php
+++ admin/ajouteArticle.php
@@ -5,12 +5,12 @@ $conn = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root')
 if(isset($_POST['titre'])){
 
   $titre =  $_POST['titre'];
-$textes =   $_POST['textes'];
+$contenu =   $_POST['contenu'];
 
-$req = $conn->prepare('INSERT INTO billets( titre, contenu, date_creation) VALUES(:titre, :textes, NOW())');
+$req = $conn->prepare('INSERT INTO billets( titre, contenu, date_creation) VALUES(:titre, :contenu, NOW())');
 $req->execute(array(
     'titre' => $titre,
-    'textes' => $textes));
+    'contenu' => $contenu));
 }
 
 
@@ -27,12 +27,17 @@ if($_SESSION['pseudo']== "admin"){
         <title>Blog</title>
         <link rel="icon" type="image/jpg" href="../images/logo.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <!-- <link href="bootstrap/css/bootstrap.css" rel="stylesheet"> -->
-        <!-- <link href="bootstrap/css/font-awesome.min.css" rel="stylesheet">  -->
-        <!-- Add icon library -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=v0f7pw8xvnajx4xwsnddsqks8qc4bshr9avi70ilks89lvf7"></script>
+<script>
+  tinymce.init({
+  selector: 'textarea',  // change this value according to your HTML
+  language_url : '../langs/fr_FR.js'  // site absolute URL
+});
+</script>
+  <script>tinymce.init({ selector:'textarea' });</script>
 	<!-- <link href="style.css" rel="stylesheet" />  -->
     <style type="text/css">
     .page{
@@ -72,106 +77,30 @@ if($_SESSION['pseudo']== "admin"){
         <br/> 
         <section class = "detail" style="margin-top:50px;">
           <div class="row">
-            <?php include("menuverticale.php"); ?>
+            <?php include("menuverticale.php");
+            echo $_POST['test']; ?>
  
             <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9"> 
       
-              <form class="form-horizontal" method="post" action="ajouteArticle.php">
-                <fieldset>
+                <h1>Ajouter un billet</h1>
+                <form method="post">
+                  <input type="text" name = "titre" placeholder ="Saisisez le titre"/>
+                  <textarea id="mytextarea" name="contenu"></textarea>
+                  <button id="button" type="submit" name="button">publier</button>
+                </form>
+             
 
-                  <!-- Form Name -->
-                  <legend>Ajouter un article</legend>
-
-                  <!-- Text input-->
-                  <div class="form-group">
-                    <label class="col-md-4 control-label" for="textinput">Titre</label>  
-                    <div class="col-md-4">
-                    <input id="textinput" name="titre" type="text" placeholder="Titre" class="form-control input-md">
-                    
-                    </div>
-                  </div>
-
-                  <!-- Textarea -->
-                  <div class="form-group">
-                    <label class="col-md-4 control-label" for="textarea">Text</label>
-                    <div class="col-md-4">                     
-                      <textarea class="form-control" id="textarea" name="textes"></textarea>
-                    </div>
-                  </div>
-
-                  <!-- Button -->
-                  <div class="form-group">
-                    <label class="col-md-4 control-label" for="singlebutton">Single Button</label>
-                    <div class="col-md-4">
-                      <button id="singlebutton" name="singlebutton" class="btn btn-primary">publier</button>
-                    </div>
-                  </div>
-
-                </fieldset>
-              </form>
-      
-            </div>
+            </div> <!--div de col -->
           </div><!--div de row -->
         </section>
 
 
-        <div class="row"> 
-          <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-md-offset-3"> 
-            <form  method="post" action="ajouteArticle.php">
-              <input type="button" value="G" style="font-weight: bold;" onclick="commande('bold');" />
-              <input type="button" value="I" style="font-style: italic;" onclick="commande('italic');" />
-              <input type="button" value="S" style="text-decoration: underline;" onclick="commande('underline');" />
-              <input type="button" value="Lien" onclick="commande('createLink');" />
-              <input type="button" value="Image" onclick="commande('insertImage');" />
-              <select onchange="commande('heading', this.value); this.selectedIndex = 0;">
-                <option value="">Titre</option>
-                <option value="h1">Titre 1</option>
-                <option value="h2">Titre 2</option>
-                <option value="h3">Titre 3</option>
-                <option value="h4">Titre 4</option>
-                <option value="h5">Titre 5</option>
-                <option value="h6">Titre 6</option>
-              </select>
-              <div id="editeur" contentEditable></div>
-              <input type="button" onclick="resultat();" value="Obtenir le HTML" />
-              <br />
-              <textarea id="resultat" name="resultat"></textarea>
-              <button id="button" name="button" class="btn btn-primary">publier</button>
-                    
-            </form>
-      </div>
-          </div>
-
-
+        
         <section class = "pied">
 
         <?php include("../footer.php"); ?>
         </section>
       </div>
-
-      <script>
-          function commande(nom, argument) {
-              
-              if (typeof argument === 'undefined') {
-                argument = '';
-              }
-              switch (nom) {
-                case "createLink":
-                  argument = prompt("Quelle est l'adresse du lien ?");
-                break;
-                case "insertImage":
-                  argument = prompt("Quelle est l'adresse de l'image ?");
-                break;
-            }
-          // Exécuter la commande
-            document.execCommand(nom, false, argument);
-          }
-
-          function resultat() {
-            document.getElementById("resultat").value = document.getElementById("editeur").innerHTML;
-          }
-
-      </script>
 
     </body>
     <?php }
