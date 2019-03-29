a821910f73da1036fc3a181c1712605145731619
mintra <studiominnie@gmail.com>
2019-03-29 23:57:08 +0100

    supprime.php utilisation de la fonction supprimer de l'objet BilletManager

--- admin/supprime.php
+++ admin/supprime.php
@@ -1,25 +1,14 @@
 <?php
 // On démarre la session AVANT d'écrire du code HTML
 session_start();
-try
-            {
-                $conn = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-            }
-            catch(Exception $e)
-            {
-                    die('Erreur : '.$e->getMessage());
-            }
-             
+
+require "BilletManager.php";
+
+
 if($_SESSION['pseudo']== "admin"){
 
-    $req = $conn->prepare('DELETE FROM billets WHERE id = :id');
-    $req->execute(array(
-    'id' => $_GET['billet']));
-
-    $req= $conn->prepare('DELETE FROM commentaires WHERE id_billet = :id');
-    $req->execute(array(
-    'id' => $_GET['billet']));
-
+    $billetManager = new BilletManager();
+    $billetManager->supprimer($_GET['billet']);
     header("Location:admin.php");
 }
 else{
