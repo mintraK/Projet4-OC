b6ac42abb4fa2990e449b75d0d3f8ddeaeceb2ea
mintra <studiominnie@gmail.com>
2019-03-31 22:35:53 +0200

    admin.php mettre en place poo pour modifier, supprimer

--- admin/admin.php
+++ admin/admin.php
@@ -22,7 +22,8 @@ if($_SESSION['pseudo']== "admin"){
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-     
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
         <style type="text/css">
                 .page{
                     overflow:hidden;
@@ -56,8 +57,8 @@ if($_SESSION['pseudo']== "admin"){
             <?php include("menuAdmin.php"); ?>
 
             </header>
-            <br/> 
-            <section class = "detail" style="margin-top:50px;">
+           
+            <section class = "detail">
                 <div class="row">
                     <?php include("menuverticale.php"); ?>
         
