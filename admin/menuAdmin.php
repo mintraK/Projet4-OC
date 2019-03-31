40124becdff2045c8e6c2f042b406a8b76099310
mintra <studiominnie@gmail.com>
2019-03-31 22:34:31 +0200

    menuAdmin.php menu responsive

--- admin/menuAdmin.php
+++ admin/menuAdmin.php
@@ -1,12 +1,15 @@
-
-
-         
-   
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="../index.php"><img src="../images/logo.jpg" style="width:150px; heigth:100px;" /></a>
-            </div>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="../index.php"><img src="../images/logo.jpg" style="width:150px; heigth:100px;" /></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+    
             
                     <ul class="nav navbar-nav pull-right">
                     <li><a class="active" href =""><span class="glyphicon glyphicon-user"></span> Bonjour <?php echo $_SESSION['pseudo'];?>  !</a> </li>
@@ -14,8 +17,9 @@
                     <li><a href="../deconnexion.php"><span class="glyphicon glyphicon-log-out"></span>  Déconnexion</a></li>
                     
                 </ul>
-        </div>  
     </div>
+  </div>
+</nav>
     </header>
  
  
