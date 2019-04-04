99e90d50864c75bcba8cc0a9de36d3485e269a46
mintra <studiominnie@gmail.com>
2019-04-04 14:13:30 +0200

    index.php transformer la page en routeur

--- index.php
+++ index.php
@@ -1,110 +1,66 @@
 <?php
-// On démarre la session AVANT d'écrire du code HTML
 session_start();
+require('controller/frontend.php');
+require('controller/backend.php');
+try {
+// ADMIN
+   if($_SESSION['pseudo'] == "admin"){
+        if ($_GET['action'] == 'dashBord'){          
+                dashBord(); 
+        }  
+   
 
-?>
+   }
+   
 
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Blog</title>
-        <link rel="icon" type="image/jpg" href="images/logo.jpg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
- 
-        <style type="text/css">
-            .page{
-                overflow:hidden;
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
             }
-            [class*="col"] { margin-bottom: 20px; }
-            img { width: 100%; }
-            body { margin-top: 10px; }
-            .fa {
-                padding: 20px;
-                font-size: 30px;
-                width: 50px;
-                text-align: center;
-                text-decoration: none;
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_SESSION['pseudo'], $_POST['comment']);
                 }
-            /* .fa-facebook {
-            color:white;
-            color: white;
-
-            }   */
-
-        </style>
-    </head>
-        
-    <body>
-        <div class="page">
-            <header>                
-                    <!-- Le menu -->
-                    <?php include("menu.php"); ?>
-            </header>
-            
-            <section id="panneau">
-                <img src="image.php" /></section>
-            
-                <h2>Dernier billet du blog :</h2>
-            
- 
-                <?php
-                // Connexion à la base de données
-                try
-                {
-                    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
                 }
-                catch(Exception $e)
-                {
-                        die('Erreur : '.$e->getMessage());
-                }
-                
-                // On récupère les 5 derniers billets
-                $req = $bdd->query('SELECT id, titre,photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 1');
-                
-                while ($donnees = $req->fetch())
-                {
-                ?>
-                <div class="row">
-                    <div class="news">
-                
-                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><img src= "<?php echo $donnees['photo'] ?>" />
-                        </div>
-                
-                        <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"><?php echo $donnees['titre']; ?></h3>
-                                    <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-                                </div>
-                                <div class="panel-body"><p>
-                                    <?php
-                                    // On affiche le contenu du billet et limite afficher max 500 carectere
-                                    echo substr($donnees['contenu'],0,500);
-                                    ?>
-                                    <br />
-                                    <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Lire la suite</a></em>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                
-                    </div>
-                </div><!--  fin div row -->
-                <?php
-                } // Fin de la boucle des billets
-                $req->closeCursor();
-                ?>
-                <br/>
-                <p><a href="tousLesArticles.php">Tous les articles</a></p>  
-            <section class = "pied">
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } 
+        elseif ($_GET['action'] == 'logout') {
+            if(isset($_SESSION['pseudo'])){
+                logoutUser();
+            }
 
-            <?php include("footer.php"); ?>
-            </section>
-        </div>
-    </body>
-</html>
+        }
+        elseif ($_GET['action'] == 'login') {
+            if(!(isset($_SESSION['pseudo']))){
+                loginUser();
+            }
+
+        } 
+        elseif ($_GET['action'] == 'register') {
+                register();
+
+        }
+    }
+    else {
+       
+        listPost();
+    }
+}
+catch(Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
+}
+
