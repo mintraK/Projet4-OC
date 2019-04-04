240400a27fc851b0aa4bdfb3fec583625b0b4a00
mintra <studiominnie@gmail.com>
2019-04-04 14:50:24 +0200

    view/frontend/footer.php ajouter les contenus pour la pied de page

---
+++ view/frontend/footer.php
@@ -0,0 +1,73 @@
+<!-- Footer -->
+<footer class="page-footer font-small" style="border-top: 4px solid black; background-color:rgb(106, 109, 109,0.1);">
+
+  <br/>
+
+    <!-- Footer Elements -->
+    <div class="container">
+      <!-- Grid row-->
+      <div class="row text-center d-flex justify-content-center pt-5 mb-3 " >
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a  href="#!" style="color:black;">A PROPOSE DE NOUS</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-3">
+        <h6 class="  text-uppercase font-weight-bold">
+          <a href="#!" style="color:black;" >SERVICES</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+      <!-- Grid column -->
+      <div class="col-md-3 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a  href="#!" style="color:black;">AIDE</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+      <!-- Grid column -->
+      <div class="col-md-3 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a  href="#!" style="color:black;">CONTACT</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+
+    <p class = "text-center" style="font-size:12px;">SUIVEZ NOUS SUR</p>
+
+    <hr class="rgba-white-light" style="margin: 0 15%;">
+    
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center" >
+      <li class="list-inline-item">
+          <a  href="https://www.facebook.com"><i class="fa fa-facebook-square fa-3x social"></i></a>	           
+      </li>
+      <li class="list-inline-item">
+        <a href="https://twitter.com"><i class="fa fa-twitter-square fa-3x social"></i></a>
+      </li>
+      <li class="list-inline-item">
+          <a href="https://plus.google.com"><i class="fa fa-google-plus-square fa-3x social" style="color:red;"></i></a>
+      </li>
+      <li class="list-inline-item">
+          <a href=""><i class="fa fa-envelope-square fa-3x social" style="color:black;"></i></a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3" style="background-color:rgb(54, 55, 56); color:white;">© 2019 Copyright: Projet4 Openclassroom
+  </div>
+  <!-- Copyright -->
+
+</footer>
+  <!-- Footer -->
