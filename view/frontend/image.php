6f1ce98c31791659e5816f7f270c5f25cb8e9238
mintra <studiominnie@gmail.com>
2019-04-04 14:51:32 +0200

    view/frontend/image.php creer l'image en php

---
+++ view/frontend/image.php
@@ -0,0 +1,12 @@
+<?php
+header ("Content-type: image/jpeg");
+$image = imagecreatefromjpeg("../../images/landscape.jpg");
+
+
+ $blanc = imagecolorallocate($image, 255, 255, 255);
+ imagestring($image, 14, 200, 300, "Jean Forteroche", $blanc);
+imagejpeg($image);
+
+
+?>
+
