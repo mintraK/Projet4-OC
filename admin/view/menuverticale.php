a07ec2c38cc43cc9bf6dfed77808ede7a5da9de0
mintra <studiominnie@gmail.com>
2019-04-05 15:07:22 +0200

    admin/view/menuverticale.php modifier les liens des rubriques

---
+++ admin/view/menuverticale.php
@@ -0,0 +1,18 @@
+ 
+ 
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 ">                
+                <ul class="nav navbar-inverse  flex-column">
+                     <li class="nav-item ">
+                         <a class="nav-link active" href="index.php">Tous les Articles</a>
+                     </li>
+                     <li class="nav-item ">
+                         <a class="nav-link active" href="index.php?action=addArticle">Ajouter un article</a>
+                     </li>  
+                     <li class="nav-item">
+                         <a class="nav-link" href="index.php?action=reportComment">Commentaires signalés</a>
+                    </li>
+                   
+                  
+                 </ul>
+        
+    </div> 
