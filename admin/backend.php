edbf1559ebbcd50709837969f077453f9fc0619e
mintra <studiominnie@gmail.com>
2019-04-05 15:03:48 +0200

    admin/backend.php ajouté des fonctions pour les commentaires signalés(afficher, supprimer, ignorer)

--- admin/backend.php
+++ admin/backend.php
@@ -1,23 +1,66 @@
 <?php
 
-// Chargement des classes
-require_once('model/PostManager.php');
-require_once('model/CommentManager.php');
+if($_SESSION['pseudo']== "admin"){
+    // Chargement des classes
+    require_once('model/PostManager.php');
+    require_once('model/CommentManager.php');
 
 
-function  dashBord(){
-      
-    $postManager = new PostManager(); // Création d'un objet
-    $posts = $postManager->getListPosts(); // Appel d'une fonction de cet objet
-    require('view/indexAdminView.php');
+    function  dashBord(){
+        
+        $postManager = new PostManager(); // Création d'un objet
+        $posts = $postManager->getListPosts(); // Appel d'une fonction de cet objet
+        require('view/indexAdminView.php');
 
+    }
+    function editArticle($postId){
+
+        $postManager = new PostManager();
+        $post = $postManager->getPost($postId);  
+        require('view/editArticleView.php');
+    
+        if(isset($_POST['titre'])){
+            $post->setTitre($_POST['titre']);
+            $post->setContenu($_POST['contenu']);
+            $postManager->editArticle($post);
+            header("Location:index.php");
+        }
+
+    }
+    function addArticle(){
+        require('model/modelAddArticle.php');
+        require('view/addArticleView.php'); 
+    }
+    function reportComment(){
+        //require('model/modelReportComment.php');
+        $commentManager = new CommentManager();
+        $comment = $commentManager->reportComment();  
+        require('view/reportCommentView.php'); 
+    }
+    function deleteComment(){
+       
+        if(isset($_GET['commentId'])){
+            $deleteComment = new CommentManager();
+            $deleteComment->deleteComment($_GET['commentId']);
+            header("Location:index.php?action=reportComment");
+        }
+        else{
+            throw new Exception('Pas de IdCommentaire !');
+        }      
+    }
+    function  ignoreComment(){
+        if(isset($_GET['commentId'])){
+            $commentManager = new CommentManager();
+            $commentManager->ignoreComment($_GET['commentId']);
+            header("Location:index.php?action=reportComment");
+        }
+        else{
+            throw new Exception('Pas de IdCommentaire !');
+        } 
+    }
 }
-function editArticle($postId){
+else 
+{
 
-    $postManager = new PostManager();
-    $post = $postManager->getPost($postId);  
-    require('view/editArticleView.php');
-    //header("Location:modifierArticle.php");
-
+    header("Location:../index.php");
 }
-
