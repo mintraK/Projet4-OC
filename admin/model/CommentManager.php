eea9f85259a616688a1543824432430223a1412e
mintra <studiominnie@gmail.com>
2019-04-05 15:20:56 +0200

    admin/model/CommentManager.php  ajouté des requêtes pour les commentaires signalés (afficher, supprimer,ignorer)

--- admin/model/CommentManager.php
+++ admin/model/CommentManager.php
@@ -4,34 +4,54 @@ require_once("CommentaireUtilisateur.php");
 
 class CommentManager extends Manager
 {
-    public function getComments($postId)
-    {
+    // public function getComments($postId)
+    // {
+    //     $db = $this->dbConnect();
+    //     $comments = $db->prepare('SELECT id, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
+    //     $comments->execute(array($postId));
+
+    //     return $comments;
+    // }
+
+    // public function postComment($postId, $author, $comment)
+    // {   $db = $this->dbConnect();
+    //     $req3 = $db->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, signaler, date_commentaire) VALUES (:id_billet, :auteur, :commentaire, :signaler, NOW())');
+    //     $req3->execute(array(
+    //         'id_billet' => $postId,
+    //         'auteur' => $author,
+    //         'commentaire' => $comment,
+    //         'signaler' => 0
+    //     ));        
+    //      header("Location:index.php?action=post&amp;id=.$postId.");
+    // }
+    public function reportComment(){
+    
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
-        $comments->execute(array($postId));
-
-        return $comments;
+        $req = $db->prepare('SELECT id, auteur, commentaire  FROM commentaires WHERE signaler = 1 ');
+        $req->execute();
+        
+        while($donnees = $req->fetch())
+        {       // creer un nouvelle objet 
+                $commentaire = new CommentaireUtilisateur($donnees); 
+                // met objet dans un tableau
+                $allcomment[] = $commentaire; 
+        }
+        return $allcomment;
+    
+    }  
+    public function deleteComment($commentId){
+        $db = $this->dbConnect(); 
+        $req= $db->prepare('DELETE FROM commentaires WHERE id = :id');
+        $req->execute(array(
+        'id' => $commentId));
     }
-
-    public function postComment($postId, $author, $comment)
-    {   $db = $this->dbConnect();
-        $req3 = $db->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, signaler, date_commentaire) VALUES (:id_billet, :auteur, :commentaire, :signaler, NOW())');
-        $req3->execute(array(
-            'id_billet' => $postId,
-            'auteur' => $author,
-            'commentaire' => $comment,
-            'signaler' => 0
+    public function ignoreComment($commentId){
+        $db = $this->dbConnect(); 
+        $req= $db->prepare('UPDATE commentaires SET signaler=0 WHERE id = :id');
+        $req->execute(array(
+            'id' => $_GET['commentId']
         ));
-        
-        
-         header("Location:index.php?action=post&amp;id=.$postId.");
-        // $db = $this->dbConnect();
-        // $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        // $affectedLines = $comments->execute(array($postId, $author, $comment));
-
-        // return $affectedLines;
     }
-
     public function dbConnect()
     {
         $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
