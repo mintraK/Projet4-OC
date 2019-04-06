9f4b732ac0020da89123666f11aa20309a8706ea
mintra <studiominnie@gmail.com>
2019-04-07 00:29:21 +0200

    view/frontend/loginView.php gérer le bootstrap

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -5,47 +5,43 @@
             <header>
                 <?php include("menu.php"); ?>
             </header>
-           <?php 
-            // if (isset($_POST['pseudo'])){
-            //     $pseudo = $_POST['pseudo'];
-            // }
-           
-           ?>
-            <form class="form-horizontal" method="post" action="index.php?action=login&amp;pseudo=<?= $_POST['pseudo'];?>">
-                <fieldset>
-             
-                <!-- Form Name -->
-                <legend  class="text-center">Se connecter au compte</legend>
+            <div class="row">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+                <form class="form-horizontal col-xs-4 col-sm-4 col-md-4 col-lg-4" method="post" action="index.php?action=login&amp;pseudo=<?= $_POST['pseudo'];?>">
+                    <fieldset>
                 
-                <!-- Text input-->
-                <div class="form-group">
-                <label class="col-md-4 control-label" for="textinput">Pseudo</label>  
-                <div class="col-md-4">
-                <input id="textinput" name="pseudo" type="text" placeholder="Pseudo" class="form-control input-md">
-                <span class="help-block">help</span>  
-                </div>
-                </div>
+                    <!-- Form Name -->
+                    <legend  class="text-center">Se connecter au compte</legend>
+                    
+                    <!-- Text input-->
+                    <div class="form-group">
+                    <label class="control-label" for="textinput">Pseudo</label>  
+                    <div class="">
+                    <input id="textinput" name="pseudo" type="text" placeholder="Pseudo" class="form-control input-md">
+                    <span class="help-block">help</span>  
+                    </div>
+                    </div>
 
-                <!-- Password input-->
-                <div class="form-group">
-                <label class="col-md-4 control-label" for="passwordinput">Mot de passe</label>
-                <div class="col-md-4">
-                    <input id="passwordinput" name="pwd" type="password" placeholder="Mot de passe" class="form-control input-md">
-                    <span class="help-block">help</span>
-                </div>
-                </div>
+                    <!-- Password input-->
+                    <div class="form-group">
+                    <label class="control-label" for="passwordinput">Mot de passe</label>
+                    <div class="">
+                        <input id="passwordinput" name="pwd" type="password" placeholder="Mot de passe" class="form-control input-md">
+                        <span class="help-block">help</span>
+                    </div>
+                    </div>
 
-                <!-- Button -->
-                <div class="form-group">
-                <label class="col-md-4 control-label" for="singlebutton"></label>
-                <div class="col-md-4">
-                    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Se connecter</button>
-                </div>
-                </div>
+                    <!-- Button -->
+                    <div class="form-group">
+                    <label class="control-label" for="singlebutton"></label>
+                    <div class="">
+                        <button id="singlebutton" name="singlebutton" class="btn btn-primary">Se connecter</button>
+                    </div>
+                    </div>
 
-                </fieldset>
-            </form>
-
+                    </fieldset>
+                </form>
+            </div>
             <section class = "pied">
                 <?php include("footer.php"); ?>
             </section>
