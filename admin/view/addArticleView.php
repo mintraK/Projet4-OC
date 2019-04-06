680452d562ae2c27b25f3689d6a92605a5720886
mintra <studiominnie@gmail.com>
2019-04-06 23:31:49 +0200

    admin/view/addArticleView.php indentation

--- admin/view/addArticleView.php
+++ admin/view/addArticleView.php
@@ -1,49 +1,33 @@
 <?php
-// On démarre la session AVANT d'écrire du code HTML
  session_start();
-
-
 ?>
-
 <?php $title = "Ajouter un billet"; ?>
-
 <?php ob_start(); ?>
 <div class="page">
         <header>  
           <?php include("menuAdmin.php"); ?>
-        </header>
-       
+        </header>     
         <section class = "detail">
           <div class="row">
-            <?php include("menuverticale.php");
-            echo $_POST['test']; ?>
- 
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9"> 
-      
+            <?php include("menuverticale.php");?>
+            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">   
                 <h1>Ajouter un billet</h1>
                 <form method="post" action="index.php?action=addArticle" enctype="multipart/form-data">
                   <input type="text" name = "titre" placeholder ="Saisisez le titre"/><br />
-
                   <label for="mon_fichier">Photo (tous formats | max. 1 Mo) :</label><br />
                   <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
                   <input type="file"  name="mon_fichier" id="mon_fichier" /><br />
                   <textarea id="mytextarea" name="contenu"></textarea>
                   <button id="button" type="submit" name="button">publier</button>
                 </form>
-             
-
             </div> <!--div de col -->
           </div><!--div de row -->
         </section>
-        
         <section class = "pied">
-
-        <?php include("../footer.php"); ?>
+          <?php include("../footer.php"); ?>
         </section>
       </div>
-
-     
 <?php  $content = ob_get_clean(); ?>
- <?php require('template.php'); ?>
+<?php require('template.php'); ?>
 
 
