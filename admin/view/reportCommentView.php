1632da426d6a6922302dac95aa3ef0ebde2c66d7
mintra <studiominnie@gmail.com>
2019-04-08 01:40:34 +0200

    admin/view/reportCommentView.php modifier des colonnes: Bootstrap

--- admin/view/reportCommentView.php
+++ admin/view/reportCommentView.php
@@ -5,19 +5,17 @@
 $title = "Commentaires signalés"; ?>
 
 <?php ob_start(); ?>
-<div class="page">      
+    <div class="page">      
           <header>  
             <?php include("menuAdmin.php"); ?>
-
-          </header>
-         
+          </header>        
           <section class = "detail" >
             <div class="row">
                 <?php include("menuverticale.php"); ?>
-                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+                <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
                   <table class="table table-bordered table-striped table-condensed">
                     <caption>
-                            <h4>Tous les commentaires</h4>
+                        <h4>Tous les commentaires</h4>
                     </caption>
                     <thead>
                       <tr>
@@ -29,30 +27,28 @@ $title = "Commentaires signalés"; ?>
                     </thead>
                     <tbody>                
                       <?php foreach($comment as $donnees) {?>  
-                            <tr>
-                              <td><?= nl2br(htmlspecialchars($donnees->auteur()));?></td>
-                              <td><?= nl2br(htmlspecialchars($donnees->commentaire()))?></td>
-                              <td>
-                                <form method= "post" action = "index.php?action=deleteComment&amp;commentId=<?=$donnees->id();?>">
-                                  <button type = "submit"><span class="glyphicon glyphicon-trash" style="color: red;"></span>                    
-                                </form>
-                              </td>
-                              <td>
-                                <form method= "post" action = "index.php?action=ignoreComment&amp;commentId=<?=$donnees->id();?>">
-                                  <button type = "submit" ><span class="glyphicon glyphicon-ok" style="color: green;"></span>   
-                                </form>
-                              </td>
-                            </tr>
-                      <?php  } ?>
+                        <tr>
+                          <td><?= nl2br(htmlspecialchars($donnees->auteur()));?></td>
+                          <td><?= nl2br(htmlspecialchars($donnees->commentaire()))?></td>
+                          <td>
+                            <form method= "post" action = "index.php?action=deleteComment&amp;commentId=<?=$donnees->id();?>">
+                              <button type = "submit"><span class="glyphicon glyphicon-trash" style="color: red;"></span>                    
+                            </form>
+                          </td>
+                          <td>
+                            <form method= "post" action = "index.php?action=ignoreComment&amp;commentId=<?=$donnees->id();?>">
+                              <button type = "submit" ><span class="glyphicon glyphicon-ok" style="color: green;"></span>   
+                            </form>
+                          </td>
+                        </tr>
+                      <?php } ?>
                     </tbody>
                   </table>
 
               </div>
             </div> <!--div de row -->
-          </section>
-         
+          </section>     
         </div>
-
 <?php  $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
 
