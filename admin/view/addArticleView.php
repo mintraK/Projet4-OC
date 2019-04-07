e394dcfcfce97c15e9137ffd71344fab80805390
mintra <studiominnie@gmail.com>
2019-04-08 01:39:19 +0200

    admin/view/addArticleView.php modifier des colonnes: Bootstrap

--- admin/view/addArticleView.php
+++ admin/view/addArticleView.php
@@ -10,7 +10,7 @@
         <section class = "detail">
           <div class="row">
             <?php include("menuverticale.php");?>
-            <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">   
+            <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">   
                 <h1>Ajouter un billet</h1>
                 <form method="post" action="index.php?action=addArticle" enctype="multipart/form-data">
                   <input type="text" name = "titre" placeholder ="Saisisez le titre"/><br />
