ab02b04b1d203d7d4112b864cf1498e59e554583
mintra <studiominnie@gmail.com>
2019-04-08 01:35:09 +0200

    view/frontend/template.php ajouter style.css dans la balisse head

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -9,36 +9,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <style type="text/css">
-            .page{
-                overflow:hidden;
-                
-            }
-            #panneau{
-                margin-top:-20px;
-            }
-            .contenu-page{
-                margin-left:4%;
-                margin-rigth:4%;
-
-            }
-            [class*="col"] { margin-bottom: 20px; }
-            img { width: 100%; }
-            /* body { margin-top: 10px; } */
-            .fa {
-                padding: 20px;
-                font-size: 30px;
-                width: 50px;
-                text-align: center;
-                text-decoration: none;
-                }
-            /* .fa-facebook {
-            color:white;
-            color: white;
-
-            }   */
-
-        </style>
+        <link rel="stylesheet" href="public/style.css">
     </head>
         
     <body>
