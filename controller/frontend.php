35da441efee8cf79a123e0915e975af47106a063
mintra <studiominnie@gmail.com>
2019-04-08 17:33:39 +0200

    controller/frontend.php indentation du code

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,40 +1,32 @@
 <?php
-
-// Chargement des classes
  require_once('model/frontend/PostManager.php');
  require_once('model/frontend/CommentManager.php');
 
 function listPost()
 {
-    $postManager = new PostManager(); // Création d'un objet
-    $posts = $postManager->getLastPost(); // Appel d'une fonction de cet objet
+    $postManager = new PostManager();
+    $posts = $postManager->getLastPost(); 
     require('view/frontend/listPostView.php');
-  
 }
-function listPosts(){
-    
-    $postManager = new PostManager(); // Création d'un objet
-    $posts = $postManager->getListPosts(); // Appel d'une fonction de cet objet
+function listPosts()
+{
+    $postManager = new PostManager(); 
+    $posts = $postManager->getListPosts(); 
     require('view/frontend/allPostsView.php');
-  
-
 }
 function post()
 {
     $postManager = new PostManager();
     $commentManager = new CommentManager();
- 
     $post = $postManager->getPost($_GET['id']);
     $comments = $commentManager->getComments($_GET['id']);
-   
     require('view/frontend/postView.php');
    
 }
 
 function addComment($postId, $author, $comment)
 {
-    $commentManager = new CommentManager();
-   
+    $commentManager = new CommentManager(); 
     $addComment = $commentManager->postComment($postId, $author, $comment);
 
     if ($addComment === false) {
