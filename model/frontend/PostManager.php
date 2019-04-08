896db2a02c2d598f3784d75f2711a9db948da097
mintra <studiominnie@gmail.com>
2019-04-08 19:15:37 +0200

    model/frontend/PostManager.php indentation du code

--- model/frontend/PostManager.php
+++ model/frontend/PostManager.php
@@ -1,8 +1,6 @@
 <?php
-// require "Manager.php";
 require_once("model/frontend/Manager.php"); 
 require_once("admin/model/Billet.php"); 
-// require "../../admin/Billet.php";
 class PostManager extends Manager
 {
     public function getLastPost()
@@ -20,28 +18,18 @@ class PostManager extends Manager
                 $allPost[] = $post; 
         }
         return  $allPost;
-       
     }
 
     public function getPost($postId)
     {
-        // $db = $this->dbConnect();
-        // $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
-        // $req->execute(array($postId));
-        // $post = $req->fetch();
-
-        // return $post;
         $db = $this->dbConnect();
         $req = $db->prepare('SELECT id, titre, photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets WHERE id = ?');
         $req->execute([
             $postId
         ]);
         $data = $req->fetch();
-        return new Billet($data);
-        
+        return new Billet($data); 
     }
-
-
     public function dbConnect()
     {
         $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
