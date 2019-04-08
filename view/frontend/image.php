20dcadec8314ff88e6cbf26bb0fd73688bce81b9
mintra <studiominnie@gmail.com>
2019-04-08 17:01:29 +0200

    view/frontend/image.php changer le chemin d'image

--- view/frontend/image.php
+++ view/frontend/image.php
@@ -1,12 +1,8 @@
 <?php
 header ("Content-type: image/jpeg");
-$image = imagecreatefromjpeg("../../images/landscape.jpg");
-
-
- $blanc = imagecolorallocate($image, 255, 255, 255);
- imagestring($image, 14, 200, 300, "Jean Forteroche", $blanc);
+$image = imagecreatefromjpeg("../../public/images/landscape.jpg");
+$blanc = imagecolorallocate($image, 255, 255, 255);
+imagestring($image, 14, 200, 300, "Jean Forteroche", $blanc);
 imagejpeg($image);
-
-
 ?>
 
