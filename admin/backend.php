791387997499f00fe504d724044d9f771e04578e
mintra <studiominnie@gmail.com>
2019-04-08 17:31:43 +0200

    admin/backend.php vérifier la fonction de supprimer le billet

--- admin/backend.php
+++ admin/backend.php
@@ -1,15 +1,15 @@
 <?php
 
 if($_SESSION['pseudo']== "admin"){
-    // Chargement des classes
+  
     require_once('model/PostManager.php');
     require_once('model/CommentManager.php');
 
 
     function  dashBord(){
         
-        $postManager = new PostManager(); // Création d'un objet
-        $posts = $postManager->getListPosts(); // Appel d'une fonction de cet objet
+        $postManager = new PostManager(); 
+        $posts = $postManager->getListPosts(); 
         require('view/indexAdminView.php');
 
     }
@@ -22,9 +22,9 @@ if($_SESSION['pseudo']== "admin"){
         if(isset($_POST['titre'])){
             $post->setTitre($_POST['titre']);
             $post->setContenu($_POST['contenu']);
-           $isedit = $postManager->editArticle($post);
+            $isEdit = $postManager->editArticle($post);
            
-            if ($isedit == 1)
+            if ($isEdit == 1)
             {
                 header("Location:index.php");
             }
@@ -40,8 +40,25 @@ if($_SESSION['pseudo']== "admin"){
         require('model/modelAddArticle.php');
         require('view/addArticleView.php'); 
     }
+    function deleteArticle(){
+        if(isset($_GET['id'])){
+            $postManager = new PostManager();
+            $isDelete = $postManager->deleteArticle($_GET['id']);
+            $commentManager = new CommentManager();
+            $isDelete2 = $commentManager->deleteCommentOfArticle($_GET['id']);
+                if($isDelete==1 && $isDelete2==1){
+                    header("Location:index.php");
+                }
+                else{
+                    throw new Exception('Impossible de supprimer le billet!');
+                }
+        }
+        else{
+            throw new Exception('Aucun numéro de billet donné !');
+        }
+       
+    }
     function reportComment(){
-        //require('model/modelReportComment.php');
         $commentManager = new CommentManager();
         $comment = $commentManager->reportComment();  
         require('view/reportCommentView.php'); 
@@ -77,6 +94,5 @@ if($_SESSION['pseudo']== "admin"){
 }
 else 
 {
-
     header("Location:../index.php");
 }
