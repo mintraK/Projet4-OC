1c928c2e6d794c79ae47bfba63e7dae6c638463d
mintra <studiominnie@gmail.com>
2019-04-08 22:55:50 +0200

    model/backend/modelRegister.php ajout des conditions et gérer des exceptions

--- model/backend/modelRegister.php
+++ model/backend/modelRegister.php
@@ -3,30 +3,38 @@
     require('model/frontend/UserManager.php');
     if(isset($_POST['pseudo']) && isset($_POST['pwd']) && isset($_POST['mail']))
     {  if (!empty($_POST['pseudo']) && !empty($_POST['pwd'])&&!empty($_POST['mail'])){
-        if($_POST['pwd']==$_POST['repwd']&& preg_match("/.+@.+\..+/", $_POST['mail'])){
-            $pseudo =  $_POST['pseudo'];
-            $mail =   $_POST['mail'];
-        
-            $user = new UserManager(); 
-            $res = $user->userExist($pseudo, $mail);
-
-            if(!$res)
-            {
-                $password_hache = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
-                $isCreate = $user->createUser($pseudo, $password_hache, $mail);
-                if( $isCreate == 1)
+        if($_POST['pwd']==$_POST['repwd']){
+            
+            if(preg_match("/.+@.+\..+/", $_POST['mail'])){
+                $pseudo =  $_POST['pseudo'];
+                $mail =   $_POST['mail'];
+            
+                $user = new UserManager(); 
+                $res = $user->userExist($pseudo, $mail);
+    
+                if(!$res)
                 {
-                    header("Location: index.php?action=login");
+                    $password_hache = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+                    $isCreate = $user->createUser($pseudo, $password_hache, $mail);
+                    if( $isCreate == 1)
+                    {
+                        header("Location: index.php?action=login");
+                    }
+                    else
+                    {
+                        throw new Exception('Impossible de creer un nouvel utilisateur !');    
+                    }  
                 }
                 else
-                {
-                    throw new Exception('Impossible de creer nouveau user !');    
-                }  
+                {   
+                    throw new Exception('L\'utilisateur existe déjà!');    
+                }
             }
             else
-            {   
-                throw new Exception('user existe déjà!');    
+            {
+                throw new Exception('L\'adresse mail n\'est pas valide !');
             }
+
     
           } 
           else
@@ -36,11 +44,8 @@
     
         }
         else {
-            throw new Exception('Remplir tous les champs stp!');
+            throw new Exception('Tous les champs ne sont pas remplis !');
         }
         
-      }
-      else{
-        // 
-      }    
+      }   
 ?>
