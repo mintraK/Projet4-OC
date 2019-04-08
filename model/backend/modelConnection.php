4c9d22751725e078eb0fd5fc2cf63a015225b8e2
mintra <studiominnie@gmail.com>
2019-04-08 19:17:27 +0200

    model/backend/modelConnection.php gerer des exceptions

--- model/backend/modelConnection.php
+++ model/backend/modelConnection.php
@@ -1,44 +1,33 @@
 <?php
-
     session_start();
     require('model/frontend/UserManager.php');
         if(isset($_POST['pseudo']) && isset($_POST['pwd'])){
-        // $conn = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-        // $pseudo = $_POST['pseudo'];
+            if (!empty($_POST['pseudo']) && !empty($_POST['pwd'])) {
 
-        //  Récupération de l'utilisateur et de son pass hashé
-        // $req = $conn->prepare('SELECT id, pwd FROM membres WHERE pseudo = :pseudo');
-        // $req->execute(array(
-        //     'pseudo' => $pseudo));
-        // $resultat = $req->fetch();
-            
-        $user = new UserManager();   
-        $res =$user->userLogin($_POST['pseudo']);
-        // Comparaison du pass envoyé via le formulaire avec la base
-        $isPasswordCorrect = password_verify($_POST['pwd'], $res->pwd());
-        
-        
-        if (empty($res))
-        {
-            echo 'Mauvais identifiant ou mot de passe !';
-        }
-        else
-        {
-            if ($isPasswordCorrect) {
-                  session_start();
-                  $_SESSION['id'] = $res->id();
-                 $_SESSION['pseudo'] = $res->pseudo();
-                
-                //  echo 'Vous êtes connecté !';
-                 header("Location:index.php");
+                $user = new UserManager();   
+                $res =$user->userLogin($_POST['pseudo']);
+                // $user= 
+                $isPasswordCorrect = password_verify($_POST['pwd'], $res->pwd());
+                if (empty($res))
+                {
+                    throw new Exception('Mauvais identifiant ou mot de passe !');
+                }
+                else
+                {
+                    if ($isPasswordCorrect) {
+                        $_SESSION['id'] = $res->id();
+                        $_SESSION['pseudo'] = $res->pseudo();
+                        header("Location:index.php");
+                    }
+                    else {
+                        throw new Exception('Mot de passe est incorect!' );
+                    }
+                }
+
             }
             else {
-                echo 'Mauvais identifiant ou mot de passe !';
+                throw new Exception('Les champss ne sont pas touts remplis!' );     
             }
-        }
-
 
         }
-        else{
 
-        }
