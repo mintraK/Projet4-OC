cb753d0a2f44882c3a2d859c540b3fa5f7e13784
mintra <studiominnie@gmail.com>
2019-04-08 22:55:20 +0200

    model/backend/modelConnection.php ajout des conditions et gérer des exceptions

--- model/backend/modelConnection.php
+++ model/backend/modelConnection.php
@@ -4,26 +4,29 @@
         if(isset($_POST['pseudo']) && isset($_POST['pwd'])){
             if (!empty($_POST['pseudo']) && !empty($_POST['pwd'])) {
 
-                $user = new UserManager();   
-                $res =$user->userLogin($_POST['pseudo']);
-                // $user= 
-                $isPasswordCorrect = password_verify($_POST['pwd'], $res->pwd());
-                if (empty($res))
-                {
-                    throw new Exception('Mauvais identifiant ou mot de passe !');
-                }
-                else
-                {
-                    if ($isPasswordCorrect) {
-                        $_SESSION['id'] = $res->id();
-                        $_SESSION['pseudo'] = $res->pseudo();
-                        header("Location:index.php");
+                $user = new UserManager(); 
+                $userexist = $user->userExist($_POST['pseudo'],"");
+                if ($userexist){
+                    $res =$user->userLogin($_POST['pseudo']); 
+                    $isPasswordCorrect = password_verify($_POST['pwd'], $res->pwd());
+                    if (empty($res))
+                    {
+                        throw new Exception('Mauvais identifiant ou mot de passe !');
                     }
-                    else {
-                        throw new Exception('Mot de passe est incorect!' );
+                    else
+                    {
+                        if ($isPasswordCorrect) {
+                            $_SESSION['id'] = $res->id();
+                            $_SESSION['pseudo'] = $res->pseudo();
+                            header("Location:index.php");
+                        }
+                        else {
+                            throw new Exception('Mot de passe est incorrect!' );
+                        }
                     }
+                }else{
+                    throw new Exception('Le pseudo n\'existe pas !' );
                 }
-
             }
             else {
                 throw new Exception('Les champss ne sont pas touts remplis!' );     
