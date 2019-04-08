df2490bca9c23afed8c1f2c2a07fd659b1f0c1ce
mintra <studiominnie@gmail.com>
2019-04-08 15:34:56 +0200

    controller/backend.php enlever des commentaires du code

--- controller/backend.php
+++ controller/backend.php
@@ -1,46 +1,39 @@
 <?php
 
-// Chargement des classes
  require_once('model/frontend/PostManager.php');
  require_once('model/frontend/CommentManager.php');
 function reportComment(){
 
-    if(isset($_POST['idCommentaire'])){
+    if(isset($_POST['idCommentaire']))
+    {
         $CommentManager = new CommentManager();
-        $CommentManager->addReportComment($_POST['idCommentaire']);
-       
-       
+        $CommentManager->addReportComment($_POST['idCommentaire']);  
         header("Location:index.php?action=post&id=".$_GET['idPost']);
-    //header("Location:index.php");
     }
 }
 function logoutUser()
 {
     $_SESSION = array();
     session_destroy();
-    // Suppression des cookies de connexion automatique
     setcookie('pseudo', '');
     setcookie('pass_hache', '');
     header("Location:index.php");
   
 }
-function loginUser(){
+function loginUser()
+{
     require('model/backend/modelConnection.php');
     require('view/frontend/loginView.php');
-  
 }
-function  register(){
-
+function  register()
+{
      require('model/backend/modelRegister.php');
      require('view/frontend/registerView.php');
 
 }
-function  dashBord(){
-    // require('admin/admin.php');
+function  dashBord()
+{
      header("Location: admin");
-    // require('model/backend/modelDashbord.php');
-    // require('view/frontend/dahBordView.php');
-
 }
 
 
