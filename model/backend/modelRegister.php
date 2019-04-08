ed33e862bbca5cd0e287ae40ec9a9f9f885f37fd
mintra <studiominnie@gmail.com>
2019-04-08 19:18:53 +0200

    model/backend/modelRegister.php gerer des exceptions

--- model/backend/modelRegister.php
+++ model/backend/modelRegister.php
@@ -2,7 +2,7 @@
     session_start();
     require('model/frontend/UserManager.php');
     if(isset($_POST['pseudo']) && isset($_POST['pwd']) && isset($_POST['mail']))
-    { 
+    {  if (!empty($_POST['pseudo']) && !empty($_POST['pwd'])&&!empty($_POST['mail'])){
         if($_POST['pwd']==$_POST['repwd']&& preg_match("/.+@.+\..+/", $_POST['mail'])){
             $pseudo =  $_POST['pseudo'];
             $mail =   $_POST['mail'];
@@ -13,9 +13,15 @@
             if(!$res)
             {
                 $password_hache = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
-                $user->createUser($pseudo, $password_hache, $mail);
-                header("Location: index.php?action=login");
-                exit();
+                $isCreate = $user->createUser($pseudo, $password_hache, $mail);
+                if( $isCreate == 1)
+                {
+                    header("Location: index.php?action=login");
+                }
+                else
+                {
+                    throw new Exception('Impossible de creer nouveau user !');    
+                }  
             }
             else
             {   
@@ -28,5 +34,13 @@
               throw new Exception('Les 2 mots de passe ne sont pas identiques!');
           }
     
+        }
+        else {
+            throw new Exception('Remplir tous les champs stp!');
+        }
+        
+      }
+      else{
+        // 
       }    
 ?>
