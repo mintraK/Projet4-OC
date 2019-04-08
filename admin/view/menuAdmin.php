d82edb5bbe04d7464b5be127ff6279f3b13a2882
mintra <studiominnie@gmail.com>
2019-04-08 17:04:20 +0200

    admin/view/menuAdmin.php changer le chemin d'image ,indentation de code

--- admin/view/menuAdmin.php
+++ admin/view/menuAdmin.php
@@ -6,17 +6,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="../index.php"><img src="../images/logo.jpg" style="width:150px; heigth:100px;" /></a>
+      <a class="navbar-brand" href="../index.php"><img src="../public/images/logo.jpg" style="width:150px; heigth:100px;" /></a>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-    
-            
-                    <ul class="nav navbar-nav pull-right">
-                    <li><a class="active" href =""><span class="glyphicon glyphicon-user"></span> Bonjour <?php echo $_SESSION['pseudo'];?>  !</a> </li>
-                    <li><a href="index.php"><span class="glyphicon glyphicon-cog"></span>Tableau de bord</a></li>
-                    <li><a href="../index.php?action=logout"><span class="glyphicon glyphicon-log-out"></span>  Déconnexion</a></li>
-                    
-                </ul>
+    <div class="collapse navbar-collapse" id="myNavbar">    
+      <ul class="nav navbar-nav pull-right">
+          <li><a class="active" href =""><span class="glyphicon glyphicon-user"></span> Bonjour <?php echo $_SESSION['pseudo'];?>  !</a> </li>
+          <li><a href="index.php"><span class="glyphicon glyphicon-cog"></span>Tableau de bord</a></li>
+          <li><a href="../index.php?action=logout"><span class="glyphicon glyphicon-log-out"></span>  Déconnexion</a></li> 
+      </ul>
     </div>
   </div>
 </nav>
