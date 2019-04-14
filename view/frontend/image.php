c3e45168b976e22a89d3f2911d54cecacd5d2d55
mintra <studiominnie@gmail.com>
2019-04-14 23:01:01 +0200

    modifier le coordonnées de texte

--- view/frontend/image.php
+++ view/frontend/image.php
@@ -2,7 +2,7 @@
 header ("Content-type: image/jpeg");
 $image = imagecreatefromjpeg("../../public/images/landscape.jpg");
 $blanc = imagecolorallocate($image, 255, 255, 255);
-imagestring($image, 14, 200, 300, "Jean Forteroche", $blanc);
+imagestring($image, 5, 1700, 450, "Jean Forteroche", $blanc);
 imagejpeg($image);
 ?>
 
