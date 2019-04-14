dd149bc8c0af1acc5a92c9ccdce303486dc9b6cf
mintra <studiominnie@gmail.com>
2019-04-14 22:58:01 +0200

    ajuster l'apparence de la page(bootstrap)

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -6,27 +6,21 @@
                 <?php include("menu.php"); ?>
             </header>
             <div class="row">
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
-                <form class="form-horizontal col-xs-4 col-sm-4 col-md-4 col-lg-4" method="post" action="index.php?action=login&amp;pseudo=<?= $_POST['pseudo'];?>">
+                <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+                <form class="form-horizontal col-xs-8 col-sm-8 col-md-4 col-lg-4" method="post" action="index.php?action=login">
                     <fieldset>
                         <legend  class="text-center">Se connecter au compte</legend>
                             <div class="form-group">
                                 <label class="control-label" for="textinput">Pseudo</label>  
-                                <div class="">
-                                    <input id="textinput" name="pseudo" type="text" placeholder="Pseudo" class="form-control input-md">
+                                <input id="textinput" name="pseudo" type="text" placeholder="Pseudo" class="form-control">
                                 </div>
-                            </div>
                             <div class="form-group">
                                 <label class="control-label" for="passwordinput">Mot de passe</label>
-                                <div class="">
-                                    <input id="passwordinput" name="pwd" type="password" placeholder="Mot de passe" class="form-control input-md">
-                                </div>
+                                <input id="passwordinput" name="pwd" type="password" placeholder="Mot de passe" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label class="control-label" for="singlebutton"></label>
-                                <div class="">
-                                    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Se connecter</button>
-                                </div>
+                                <button id="singlebutton" name="singlebutton" class="btn btn-primary">Se connecter</button>
                             </div>
                     </fieldset>
                 </form>
