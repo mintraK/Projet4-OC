c698e354ef11d05d94fc7ace067a5b07e7f71bf8
mintra <studiominnie@gmail.com>
2019-04-14 22:51:52 +0200

    modifier des requêtes(mettre que des requŝtes preparer)

--- model/frontend/PostManager.php
+++ model/frontend/PostManager.php
@@ -2,26 +2,17 @@
 require_once("admin/model/Billet.php"); 
 class PostManager 
 {   
-    private $_db;
-    public function __construct(){
-        try
-        {
-            $this->_db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-        }
-        catch(Exception $e)
-        {
-            die('Erreur : '.$e->getMessage());
-        }
-    }
     public function getLastPost()
-    {
-        $req = $this->_db->query('SELECT id, titre,photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets ORDER BY date_creation DESC LIMIT 0, 1');
+    {   $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, titre,photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets ORDER BY date_creation DESC LIMIT 0, 1');
+        $req->execute();
         $data = $req->fetch();
         return new Billet($data);
     }
     public function getListPosts(){
-       
-        $req = $this->_db->query('SELECT id, titre, photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets ORDER BY date_creation DESC');
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, titre, photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets ORDER BY date_creation DESC');
+        $req->execute();
         while( $data = $req->fetch()){     
                 $post = new Billet($data); 
                 $allPost[] = $post; 
@@ -30,12 +21,17 @@ class PostManager
     }
 
     public function getPost($postId)
-    {
-        $req = $this->_db->prepare('SELECT id, titre, photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets WHERE id = ?');
+    {   $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, titre, photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets WHERE id = ?');
         $req->execute([
             $postId
         ]);
         $data = $req->fetch();
         return new Billet($data); 
+    }
+    public function dbConnect()
+    {
+        $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+        return $db;
     }
 }
