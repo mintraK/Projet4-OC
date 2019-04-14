a8b95982337516109041127fffb4c56927755244
mintra <studiominnie@gmail.com>
2019-04-14 22:56:27 +0200

    ajuster l'apparence de la page(bootstrap) et mettre alt pour les balises d'image

--- view/frontend/allPostsView.php
+++ view/frontend/allPostsView.php
@@ -4,12 +4,12 @@
         <header> 
             <?php include("menu.php"); ?>
         </header>
-        <section id="panneau"><img src="view/frontend/image.php" /></section> 
+        <section id="panneau"><img src="view/frontend/image.php" alt="photo"/></section> 
         <h2 class="text-center">Tous les billets du blog :</h2>
             <?php foreach ($posts as $donnees) {?>
             <div class="row">
                 <div class="news">
-                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style = "padding-top:15px;"><img src= "<?= $donnees->photo(); ?>" />
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><img src= "<?= $donnees->photo(); ?>" alt="photo-du-billet"/>
                     </div>
                     <div class="col-xs-9 col-sm-9 col-md-8 col-lg-8">
                         <div class="panel panel-primary">
@@ -17,16 +17,15 @@
                                 <h3 class="panel-title"><?= $donnees->titre(); ?></h3>
                                 <em>le <?= $donnees->dateCreation(); ?></em>
                             </div>
-                            <div class="panel-body"><p>
+                            <div class="panel-body">
                                 <?= substr($donnees->contenu(),0,400);?>
                                 <br />
                                 <em><a href="index.php?action=post&amp;id=<?= $donnees->id();?>">Lire la suite</a></em>
-                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div><
+            </div>
             <?php } ?>
             <br/>
             <section class = "pied">
