1534279c4669e05b1d9cb35bf0b1439668570f86
mintra <studiominnie@gmail.com>
2019-04-14 22:58:20 +0200

    ajuster l'apparence de la page(bootstrap) et mettre alt pour les balises d'image

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -6,44 +6,47 @@
     <header>                
         <?php include("menu.php"); ?>
     </header>
-    <p><a href="index.php?action=listPosts">Retour à la liste des billets</a></p>
+    <section id = "panneau">
+        <img src="<?= $post->photo(); ?>" alt="photo"/>    
+    </section>
+    <br/>
+    <p><a href="index.php?action=listPosts">Retour à la liste des billets</a></p><br/>
     <div class="contenu-page">
     
             <div class = "row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style = "margin:20px;"> 
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
                     <div class="panel panel-primary">
                         <div class="panel-heading">
                             <h3 class="panel-title"><?= $post->titre(); ?><em>le <?= $post->dateCreation(); ?></em></h3>
                         </div>
-                        <div class="panel-body"><p> <?= nl2br($post->contenu()); ?></p></div>
+                        <div class="panel-body"><?= nl2br($post->contenu()); ?></div>
                     </div>        
                 </div>
             </div>
 
             <div class = "row">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
-                    <h2>Commentaires</h2>
-                        <?php while ($comment = $comments->fetch()){ ?>
-                            <p><strong>
+                    <h2>Commentaires</h2> 
+                        <?php foreach ($comments as $comment){ ?>
                                 <div class="panel panel-info">
                                     <div class="panel-heading">
-                                        <form method= "post" action = "index.php?action=reportComment&amp;idComment=<?= $comment['id']; ?>&amp;idPost=<?= $comment['id_billet']?>">
-                                            <h3 class="panel-title"><?= htmlspecialchars($comment['auteur']); ?></h3>
-                                            <input type = "hidden" name = "idCommentaire" value =  "<?= $comment['id']; ?>" />
-                                            </strong> le <?= $comment['date_commentaire_fr']; ?>  <button type = "submit" style="color:red;" ><abbr title="Signaler"><span class="glyphicon glyphicon-exclamation-sign"></span></abbr>
+                                        <form method= "post" action = "index.php?action=reportComment&amp;idComment=<?= $comment->id(); ?>&amp;idPost=<?= $comment->idBillet() ?>">
+                                            <h3 class="panel-title"><?= htmlspecialchars($comment->auteur()); ?> </h3>
+                                            <input type = "hidden" name = "idCommentaire" value =  "<?= $comment->id(); ?>" />
+                                            le <?= $comment->dateCommentaire();?><?= $comment->idBillet()?><button type = "submit" style="color:red;"><abbr title="Signaler"><span class="glyphicon glyphicon-exclamation-sign"></span></abbr></button>
                                         </form>
-                                    </div></p> 
-                                    <p><div class="panel-body"><?= nl2br(htmlspecialchars($comment['commentaire'])); ?></div></p></div>
+                                    </div>
+                                    <div class="panel-body"><?= nl2br(htmlspecialchars($comment->commentaire())); ?></div></div>
                         <?php } ?>
                 </div>
             </div>
             
             <div class = "row">
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 "> </div>
-                <div class="col-xs-6 col-sm-6 col-md-6  col-lg-6 "> 
+            <div class="col-xs-2 col-sm-2 col-md-3 col-lg-3 "> </div>
+                <div class="col-xs-8 col-sm-8 col-md-6  col-lg-6 "> 
                         <form class="form-group" method = "post" action = "index.php?action=addComment&amp;id=<?= $post->id(); ?>">
                             <fieldset>
-                                <legend class="text-center"> <h2>Ajouter un commentaire</h2></legend>
+                                    <legend class="text-center"> Ajouter un commentaire</legend>
                                     <div class="form-group">   
                                         <label class="control-label" for="textinput">Prénom</label>
                                         <input id="textinput" name="author" type="text" placeholder="<?= $_SESSION['pseudo'];?>" class="form-control input-md"> 
@@ -54,16 +57,17 @@
                                     </div>
                                     <div class="form-group text-center">
                                         <button id="Envoyer" type = "submit" name="envoyerCommentaire" class="btn btn-primary">Envoyer</button> 
-                                        </div>
+                                    </div>
                             </fieldset>
                         </form>
                     </div>
-            </div>
+            </div> 
+        </div>
         <section class = "pied">
 
           <?php include("footer.php"); ?>
         </section>
-    </div>
+   
 </div>
     
 <?php $content = ob_get_clean(); ?>
