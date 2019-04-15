d57d459e6299fc6f4189b6f71589a2e1a9adb11d
mintra <studiominnie@gmail.com>
2019-04-15 23:24:11 +0200

    modifier le menuverticale et enlever le bootstrap

--- admin/view/menuverticale.php
+++ admin/view/menuverticale.php
@@ -1,5 +1,5 @@
  
- 
+<!--  
     <div class="col-xs-11 col-sm-11 col-md-3 col-lg-3 ">                
                 <ul class="nav navbar-inverse  flex-column text-center">
                      <li class="nav-item ">
@@ -12,4 +12,25 @@
                          <a class="nav-link" href="index.php?action=reportComment">Commentaires signalés</a>
                     </li>        
                  </ul>
-    </div> 
+    </div>  -->
+<!--     
+    <div id = "menu-gauche" class="col-xs-11 col-sm-11 col-md-3 col-lg-3 ">                
+                <ul class="nav">
+                     <li>
+                         <a href="index.php">Tous les Articles</a>
+                     </li>
+                     <li>
+                         <a href="index.php?action=addArticle">Ajouter un article</a>
+                     </li>  
+                     <li>
+                         <a  href="index.php?action=reportComment">Commentaires signalés</a>
+                    </li>        
+                 </ul>
+    </div>  -->
+    
+<ul id="menu-vertical">
+    <li><a href="index.php">Tous les Articles</a></li>
+    <li><a href="index.php?action=addArticle">Ajouter un article</a></li>
+    <li><a href="index.php?action=reportComment">Commentaires signalés</a></li>
+</ul>
+	
