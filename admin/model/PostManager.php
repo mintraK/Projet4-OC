cc8e48bc237b5973c0d3e3ba60e7b20b902ea55d
mintra <studiominnie@gmail.com>
2019-04-15 23:26:59 +0200

    vérifier les fonctions

--- admin/model/PostManager.php
+++ admin/model/PostManager.php
@@ -20,8 +20,20 @@
         $req->execute([
             $postId
         ]);
-        $data = $req->fetch();
-        return new Billet($data);
+        $num_of_rows = $req->rowCount() ;
+        if ($num_of_rows > 0)
+        {
+            $data = $req->fetch();
+            return new Billet($data); 
+        }
+        else {
+            $req = $db->prepare('SELECT id, titre, photo, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS datecreation FROM billets WHERE id = ?');
+            $req->execute([
+                1
+            ]);
+            $data = $req->fetch();
+            return new Billet($data);
+        } 
         
     }
     public function editArticle(Billet $billet){
@@ -30,12 +42,12 @@
         $photo =   $billet->photo();
         $contenu =   $billet->contenu();
         
-            $req = $db->prepare('UPDATE billets SET titre = :titre, contenu = :contenu, date_creation= NOW() WHERE id = :id ');
-            $isEdit =  $req->execute(array(
-            'titre' => $titre,
-            'contenu' => $contenu,
-            'id' => $billet->id()));
-            return $isEdit;
+        $req = $db->prepare('UPDATE billets SET titre = :titre, contenu = :contenu, date_creation= NOW() WHERE id = :id ');
+        $isEdit =  $req->execute(array(
+        'titre' => $titre,
+        'contenu' => $contenu,
+        'id' => $billet->id()));
+        return $isEdit;
 
     }
 
