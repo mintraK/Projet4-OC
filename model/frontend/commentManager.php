47e6bb951a57ab4b925421432fa298b426d70ae9
mintra <studiominnie@gmail.com>
2019-04-16 16:51:15 +0200

    vérifier des fonctions

--- model/frontend/commentManager.php
+++ model/frontend/commentManager.php
@@ -1,35 +1,40 @@
 <?php
-require_once("model/frontend/Manager.php"); 
-require_once("admin/CommentaireUtilisateur.php"); 
+// require_once("model/frontend/Manager.php"); 
+require_once("admin/model/CommentaireUtilisateur.php"); 
 
-class CommentManager extends Manager
-{
+ class CommentManager //extends Manager
+{    
+    
     public function getComments($postId)
     {
-        $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
-        $comments->execute(array($postId));
-
+         $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
+        $req->execute(array($postId));
+        while($data = $req->fetch())
+        {   
+            $commentaire = new CommentaireUtilisateur($data);
+            $comments[] = $commentaire; 
+        }
         return $comments;
     }
-
+    public function addReportComment($idCommentaire)
+    {  
+        $db = $this->dbConnect();     
+        $req = $db->prepare('UPDATE commentaires SET signaler =1 WHERE id = ?');
+        $isReport = $req->execute(array($idCommentaire));
+        return $isReport;  
+    }
     public function postComment($postId, $author, $comment)
-    {   $db = $this->dbConnect();
+    {   
+         $db = $this->dbConnect();
         $req3 = $db->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, signaler, date_commentaire) VALUES (:id_billet, :auteur, :commentaire, :signaler, NOW())');
-        $req3->execute(array(
+        $isPostComment = $req3->execute(array(
             'id_billet' => $postId,
             'auteur' => $author,
             'commentaire' => $comment,
             'signaler' => 0
         ));
-        
-        
-         header("Location:index.php?action=post&amp;id=.$postId.");
-        // $db = $this->dbConnect();
-        // $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        // $affectedLines = $comments->execute(array($postId, $author, $comment));
-
-        // return $affectedLines;
+        return $isPostComment;      
     }
 
     public function dbConnect()
