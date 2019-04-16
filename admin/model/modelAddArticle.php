3219f47610a5c38c66c36fbf76a900e309ec5c3f
mintra <studiominnie@gmail.com>
2019-04-16 16:48:11 +0200

    modifier la condition pour tester le titre s'il est vide

--- admin/model/modelAddArticle.php
+++ admin/model/modelAddArticle.php
@@ -2,58 +2,64 @@
 session_start();
 
 if(isset($_POST['titre'])){
-  
-  $titre =  $_POST['titre'];
-  $contenu =   $_POST['contenu'];
+  if(!empty($_POST['titre'])){
+      $titre =  $_POST['titre'];
+    $contenu =   $_POST['contenu'];
 
-// Ajout contenu avec Image
-//ajouter une photo
-if (isset($_FILES['mon_fichier']) && $_FILES['mon_fichier']['error'] === UPLOAD_ERR_OK) {
+    // Ajout contenu avec Image
+    //ajouter une photo
+    if (isset($_FILES['mon_fichier']) && $_FILES['mon_fichier']['error'] === UPLOAD_ERR_OK) {
 
-  $fileTmpPath = $_FILES['mon_fichier']['tmp_name'];
-  $fileName = $_FILES['mon_fichier']['name'];
-  $fileSize = $_FILES['mon_fichier']['size'];
-  $fileType = $_FILES['mon_fichier']['type'];
-  $fileNameCmps = explode(".", $fileName);
-  $fileExtension = strtolower(end($fileNameCmps));
+      $fileTmpPath = $_FILES['mon_fichier']['tmp_name'];
+      $fileName = $_FILES['mon_fichier']['name'];
+      $fileSize = $_FILES['mon_fichier']['size'];
+      $fileType = $_FILES['mon_fichier']['type'];
+      $fileNameCmps = explode(".", $fileName);
+      $fileExtension = strtolower(end($fileNameCmps));
 
-  $ExtensionsAutorise = array('jpg', 'png');
-  if (in_array($fileExtension, $ExtensionsAutorise)) {
-      $newFileName = $fileName;
-      $uploadFileDir = '../public/images/';
-      $dest_path = $uploadFileDir . $newFileName;
-      
-      if(move_uploaded_file($fileTmpPath, $dest_path))
-      {
+      $ExtensionsAutorise = array('jpg', 'png');
+      if (in_array($fileExtension, $ExtensionsAutorise)) {
+          $newFileName = $fileName;
+          $uploadFileDir = '../public/images/';
+          $dest_path = $uploadFileDir . $newFileName;
+          
+          if(move_uploaded_file($fileTmpPath, $dest_path))
+          {
+            $postManager = new PostManager();
+            $post = new Billet([
+              'titre' => $titre,
+              'photo' => 'public/images/'.$newFileName,
+              'contenu' => $contenu
+            ]);
+            $isadd = $postManager->addArticle($post);
+            if($isadd = 0)
+            {
+              throw new Exception("Impossible d'ajouter un articles");
+            }
+        
+          }
+        } 
+      }
+    else 
+      { 
+        // Ajout Contenu Sans imgae 
         $postManager = new PostManager();
         $post = new Billet([
           'titre' => $titre,
-          'photo' => 'public/images/'.$newFileName,
           'contenu' => $contenu
         ]);
-        $postManager->addArticle($post);
-        if($isadd = 0)
+        $isadd =  $postManager->addArticle($post);
+        if($isadd == 0)
         {
           throw new Exception("Impossible d'ajouter un articles");
         }
-    
-      }
-    } 
-  }
-else 
-  { 
-    // Ajout Contenue Sans imgae 
-    $postManager = new PostManager();
-    $post = new Billet([
-      'titre' => $titre,
-      'contenu' => $contenu
-    ]);
-    $isadd =  $postManager->addArticle($post);
-    if($isadd == 0)
-    {
-      throw new Exception("Impossible d'ajouter un articles");
-    }
 
+      }
+      header("Location:index.php");
   }
-   header("Location:index.php");
+  else{
+      throw new Exception("Pas de titre!");
+  }
+    
 }
+
