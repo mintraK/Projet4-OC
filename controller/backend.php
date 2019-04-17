61e6e29127eb464d96407f2573bd1eb7630cf698
mintra <studiominnie@gmail.com>
2019-04-17 16:43:31 +0200

    deplacer le code de modelConnection et modelReister dans le fichier pour mvc

--- controller/backend.php
+++ controller/backend.php
@@ -2,6 +2,7 @@
 
  require_once('model/frontend/PostManager.php');
  require_once('model/frontend/CommentManager.php');
+ require('model/frontend/UserManager.php');
 function reportComment(){
 
     if(isset($_POST['idCommentaire']))
@@ -26,13 +27,88 @@ function logoutUser()
   
 }
 function loginUser()
-{
-    require('model/backend/modelConnection.php');
+{  
+        if(isset($_POST['pseudo']) && isset($_POST['pwd'])){
+            if (!empty($_POST['pseudo']) && !empty($_POST['pwd'])) {
+
+                $user = new UserManager(); 
+                $userexist = $user->userExist($_POST['pseudo'],"");
+                if ($userexist){
+                    $res =$user->userLogin($_POST['pseudo']); 
+                    $isPasswordCorrect = password_verify($_POST['pwd'], $res->pwd());
+                    if (empty($res))
+                    {
+                        throw new Exception('Mauvais identifiant ou mot de passe !');
+                    }
+                    else
+                    {
+                        if ($isPasswordCorrect) {
+                            $_SESSION['id'] = $res->id();
+                            $_SESSION['pseudo'] = $res->pseudo();
+                            header("Location:index.php");
+                        }
+                        else {
+                            throw new Exception('Mot de passe est incorrect!' );
+                        }
+                    }
+                }else{
+                    throw new Exception('Le pseudo n\'existe pas !' );
+                }
+            }
+            else {
+                throw new Exception('Les champss ne sont pas touts remplis!' );     
+            }
+
+        }
+
+
     require('view/frontend/loginView.php');
 }
 function  register()
 {
-     require('model/backend/modelRegister.php');
+    if(isset($_POST['pseudo']) && isset($_POST['pwd']) && isset($_POST['mail']))
+    {  if (!empty($_POST['pseudo']) && !empty($_POST['pwd'])&&!empty($_POST['mail'])){
+        if($_POST['pwd']==$_POST['repwd']){
+            
+            if(preg_match("/.+@.+\..+/", $_POST['mail'])){
+                $pseudo =  $_POST['pseudo'];
+                $mail =   $_POST['mail'];
+            
+                $user = new UserManager(); 
+                $res = $user->userExist($pseudo, $mail);
+    
+                if(!$res)
+                {
+                    $password_hache = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+                    $isCreate = $user->createUser($pseudo, $password_hache, $mail);
+                    if( $isCreate == 1)
+                    {
+                        header("Location: index.php?action=login");
+                    }
+                    else
+                    {
+                        throw new Exception('Impossible de creer un nouvel utilisateur !');    
+                    }  
+                }
+                else
+                {   
+                    throw new Exception('L\'utilisateur existe déjà!');    
+                }
+            }
+            else
+            {
+                throw new Exception('L\'adresse mail n\'est pas valide !');
+            }
+          } 
+          else
+          { 
+              throw new Exception('Les 2 mots de passe ne sont pas identiques!');
+          }
+        }
+        else {
+            throw new Exception('Tous les champs ne sont pas remplis !');
+        } 
+      }   
      require('view/frontend/registerView.php');
 
 }
